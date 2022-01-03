8cd4c7bebc1b0f17e21250907fa7f0b57ba2b170
Sarthak Yadav <sarthak.yadav3@gmail.com>
2022-01-03 19:41:58 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="audio_data_utils",
+    version="0.0.1",
+    author='Sarthak Yadav',
+    description="Audio data loading utils for personal research",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SarthakYadav/audio_data_utils",
+    # package_dir={"": ""},
+    packages=[
+        "audio_utils",
+        "audio_utils.common"
+    ],
+    python_requires=">=3.8"
+)
