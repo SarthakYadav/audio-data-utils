7ce93e1af48ad5e8271afc0e1e79ff46f80f5c2a
Sarthak Yadav <sarthak.yadav3@gmail.com>
2022-01-05 18:49:23 +0000

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="audio-data-utils",
-    version="0.0.1",
+    version="0.0.2",
     author='Sarthak Yadav',
     description="Audio data loading utils for personal research",
     long_description=long_description,
